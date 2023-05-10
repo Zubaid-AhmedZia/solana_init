2a256dd5549f61d8a33d7d2186a1272c0d0b7bcf
Zubaid-AhmedZia <ziazubaidahmed@gmail.com>
2023-05-11 02:50:07 +0500

    starting solana..loving typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,46 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js' 
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [info, setInfo] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+
+    try{
+
+    const key = new web3.PublicKey(address);  
+    setAddress(key.toBase58())
+
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+
+    connection.getBalance(key).then(balance => {
+      setBalance(balance/web3.LAMPORTS_PER_SOL)
+    })
+
+    connection.getAccountInfo(key).then(info => {
+      if (!info?.executable) {
+
+        setInfo('Nope')
+
+        
+      } else {
+
+        setInfo('Yasss')
+        
+      }
+      
+    })
+  } catch(error) {
+    setAddress('')
+    setBalance(0)
+      alert(error)
+  }
+    // setAddress(address)
+    // setBalance(1000)
   }
 
   return (
@@ -23,6 +55,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? : ${info} `}</p>
       </header>
     </div>
   )
